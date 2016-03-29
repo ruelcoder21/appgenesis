b953d4b36e63df1564ee2517f30786489a161c75
ruelcoder21 <arktikblue@gmail.com>
2016-03-28 23:32:22 -0700

    Added files via upload

---
+++ Gemfile
@@ -0,0 +1,31 @@
+source 'https://rubygems.org'
+
+
+gem 'rails', '4.0.2'
+# gem 'sqlite3'
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+group :doc do
+  gem 'sdoc', require: false
+end
+
+
+
+
+
+
+###Ruel Additional 
+gem 'bootstrap-sass'
+gem 'devise', '~> 3.5.3'
+
+group :development, :test do
+     gem 'sqlite3'
+end
+
+group :production do
+     gem 'pg'
+     gem 'rails_12factor'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pinteresting2::Application.config.secret_key_base = '303afb847873e4cf6b3c0b9c9ff83bad64b66734805b12a41e1b90dc7d498f4a6a3d138ad7b4e487a881b604fc2a92b9530fbdc3046eab46b2399aeb00f92534'
